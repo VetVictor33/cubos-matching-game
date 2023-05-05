fbaf6e00d461e467da14a989a0c0a474d70b237d
Victor Feliciano <victorjfeliciano@gmail.com>
2023-05-05 16:48:57 -0300

    improving style and more

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import CardBack from './assets/card-back.png'
 import Congrats from './assets/congrats.png'
 import { useState } from 'react'
 import './App.css'
+import Unclickable from './assets/unclickable.svg'
 
 function gambit(cards) {
   let index = cards.length;
@@ -12,34 +13,25 @@ function gambit(cards) {
     const randomIndex = Math.floor(Math.random() * index--);
     [cards[index], cards[randomIndex]] = [cards[randomIndex], cards[index]];
   }
-  /*
-  for (let index = cards.length; index; index--) {
-
-    const randomIndex = Math.floor(Math.random() * index);
-
-    const card = cards[index - 1];
-
-    cards[index - 1] = cards[randomIndex];
-
-    cards[randomIndex] = card;
-  }
-  */
   return cards
 }
+
+const unClickebleCard = { slug: 'unclickable', image: Unclickable, turned: true }
 
 export default function App() {
   const [cardsArray, setCards] = useState(gambit(cards));
   const [isFirstCard, setIsFirstCard] = useState(true);
   const [secondCard, setSecondCard] = useState(undefined);
   const [locked, setLocked] = useState(false);
+  const [count, setCount] = useState(0);
+  const [win, setWin] = useState(false);
+  const [totalWins, setTotalWins] = useState(0);
 
-
-
-  function handleCardClick(id) {
+  function handleCardClick(e, id) {
     if (locked) return
     const localCards = [...cardsArray];
     const card = localCards.find((card) => card.id === id);
-    if (card.turned) return
+    if (card.turned || card.slug === 'unclickable') return
     setLocked(true);
     card.turned = !card.turned;
     if (isFirstCard) {
@@ -50,14 +42,22 @@ export default function App() {
       return
     }
     setIsFirstCard(true);
+    let localCount = count;
     if (card.slug === secondCard.slug) {
       const firstCardIndex = localCards.findIndex((localCard) => localCard.id === card.id);
-      localCards.splice(firstCardIndex, 1);
+      localCards.splice(firstCardIndex, 1, { ...unClickebleCard, id: Math.random() });
       const secondCardIndex = localCards.findIndex((localCard) => localCard.id === secondCard.id);
-      localCards.splice(secondCardIndex, 1);
+      localCards.splice(secondCardIndex, 1, { ...unClickebleCard, id: Math.random() });
       setTimeout(() => {
         setCards(localCards)
         setLocked(false);
+        localCount++;
+        setCount(localCount);
+        if (localCount === (cards.length / 2)) {
+          setWin(true);
+          setTotalWins(totalWins + 1);
+        };
+        console.log(`Local count: ${localCount}, count: ${count}`)
       }, 1000);
       return
     }
@@ -76,22 +76,34 @@ export default function App() {
     setCards(resetCards);
     setSecondCard(undefined);
     setIsFirstCard(true);
+    setCount(0);
+    setWin(false);
   }
 
   return (
     <div className='App'>
       <aside>
         <img src={Logo} alt='logo' />
+        <div className='score'>
+          <div>
+            <h1>Matches:</h1>
+            <p>{count}</p>
+          </div>
+          <div>
+            <h1>Wins:</h1>
+            <p>{totalWins}</p>
+          </div>
+        </div>
         <button onClick={() => reset()}>Reset</button>
       </aside>
       <main>
-        {!!cardsArray.length && cardsArray.map((card) => {
+        {!win ? cardsArray.map((card) => {
           const src = card.turned ? card.image : CardBack;
           return (
-            <img onClick={() => handleCardClick(card.id)} key={card.id} src={src} alt={card.slug} className='card' />
+            <img onClick={(e) => handleCardClick(e, card.id)} key={card.id} src={src} alt={card.slug} className={`card ${card.slug}`} />
           )
-        })}
-        {!cardsArray.length && <div className='congrats'> < img src={Congrats} /></div>}
+        })
+          : <div className='congrats'> < img src={Congrats} /></div>}
       </main>
     </div>
   );
